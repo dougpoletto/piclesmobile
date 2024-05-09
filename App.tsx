cd0db6d782b9e6202c8d060b9bfad1defd62af1a
dougpoletto <polettodoug@hotmail.com>
2024-05-08 23:25:22 -0300

    Criação dos estilos padrões e estilização da página home

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ShelterScreen from './src/pages/admin/ShelterScreen';
 import HomeScreen from './src/pages/home/HomeScreen';
 import PetsScreen from './src/pages/pets/PetsScreen';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Shelter" component={ShelterScreen} />
-        <Stack.Screen name="Pets" component={PetsScreen} />
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen options={{drawerLabel: "Início"}} name="Home">
+          {(props) => <HomeScreen {...props}/>}
+        </Drawer.Screen>
+        <Drawer.Screen name="Shelter" component={ShelterScreen} />
+        <Drawer.Screen name="Pets" component={PetsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
---
+++ src/layout/Estilos.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native"
+
+export const StylesDefault = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  viewHomeButtons: {
+    margin: 3,
+    flexDirection: "column",
+    width: "60%",
+  },
+  imageHome: {
+    width: 256,
+    height: 248
+  },
+  buttonHome: {
+    borderRadius: 1,
+    margin: 3
+  }
+})
